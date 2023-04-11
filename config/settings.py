10b5c92ae04860de7823cf0bb769857b155ddf59
fateme_parand <fateme.parand94@gmail.com>
2023-04-11 15:36:24 +0330

    user custom email for registration

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
     # third_party_apps
     'crispy_forms',
