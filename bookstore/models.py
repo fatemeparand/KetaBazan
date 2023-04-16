b231eed60db7835d0f4656af67866f128cf25846
fateme_parand <fateme.parand94@gmail.com>
2023-04-16 11:47:16 +0330

    translate some words with i18n

--- bookstore/models.py
+++ bookstore/models.py
@@ -7,12 +7,18 @@ class BookAuthor(models.Model):
     author_name = models.CharField(max_length=200, verbose_name=_('book author'))
     biography = models.TextField(verbose_name=_('biography'), blank=True)
 
+    class Meta:
+        verbose_name_plural = _('BookAuthors')
+
     def __str__(self):
         return self.author_name
 
 
 class BookTranslator(models.Model):
     translator_name = models.CharField(max_length=200, verbose_name=_('translator name'))
+
+    class Meta:
+        verbose_name_plural = _('BookTranslators')
 
     def __str__(self):
         return self.translator_name
@@ -22,12 +28,18 @@ class BookPublisher(models.Model):
     publisher_name = models.CharField(max_length=200, verbose_name=_('publisher name'))
     short_description = models.CharField(max_length=500, blank=True, verbose_name=_('about publisher'))
 
+    class Meta:
+        verbose_name_plural = _('BookPublishers')
+
     def __str__(self):
         return self.publisher_name
 
 
 class Subject(models.Model):
     subject = models.CharField(verbose_name=_('book subject'))
+
+    class Meta:
+        verbose_name_plural = _('Subjects')
 
     def __str__(self):
         return self.subject
@@ -48,8 +60,9 @@ class Book(models.Model):
                                        verbose_name=_('publisher name'))
     publication_year = models.PositiveIntegerField(verbose_name=_('publication year'))
     book_introduction = models.TextField(verbose_name=_('book introduction'))
-    short_description = models.CharField(max_length=500, blank=True, verbose_name=_('book description'))
+    short_description = models.CharField(max_length=500, blank=True, verbose_name=_('short description'))
     book_price = models.PositiveIntegerField(verbose_name=_('book price'))
+    book_pages = models.PositiveIntegerField(verbose_name=_('number of pages'))
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name='subjects',
                                 verbose_name=_('book subject'))
 
@@ -59,6 +72,9 @@ class Book(models.Model):
 
     datetime_created = models.DateTimeField(auto_now_add=True, verbose_name=_('creation date'))
     datetime_modified = models.DateTimeField(auto_now=True, verbose_name=_('Modification date'))
+
+    class Meta:
+        verbose_name_plural = _('Books')
 
     def __str__(self):
         return f"book introduction: {self.book_name}, {self.book_author}, {self.subject}"
