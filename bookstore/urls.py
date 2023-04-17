e4107f0e5e5452070b52fa776c3bc349847ec9c3
fateme_parand <fateme.parand94@gmail.com>
2023-04-17 21:19:42 +0330

    convert functional view to class-base viwe

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -5,9 +5,9 @@ app_name = 'bookstore'
 
 
 urlpatterns = [
-    path('', views.book_list, name='book_list'),
-    path('<int:pk>/', views.book_detail, name='book_detail'),
-    path('new-book/', views.book_create, name='book_create'),
-    path('<int:pk>/update/', views.book_update, name='book_update'),
-    path('<int:pk>/delete/', views.book_delete, name='book_delete'),
+    path('', views.BookList.as_view(), name='book_list'),
+    path('<int:pk>/', views.BookDetail.as_view(), name='book_detail'),
+    path('new-book/', views.BookCreate.as_view(), name='book_create'),
+    path('<int:pk>/update/', views.BookUpdate.as_view(), name='book_update'),
+    path('<int:pk>/delete/', views.BookDelete.as_view(), name='book_delete'),
 ]
--- bookstore/views.py
+++ bookstore/views.py
@@ -1,52 +1,87 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views import generic
+from django.urls import reverse_lazy
 from .forms import BookCreationForm
 from .models import Book
 
 
-def book_list(request):
-    book_lists = Book.objects.filter(active=True).order_by('-datetime_modified')
-    context = {'book_lists': book_lists}
-    return render(request, 'bookstore/book_list.html', context)
+class BookList(generic.ListView):
+    context_object_name = 'book_lists'
+    template_name = 'bookstore/book_list.html'
+
+    def get_queryset(self):
+        return Book.objects.filter(active=True).order_by('-datetime_modified')
 
 
-def book_detail(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-
-    content = {'book': book}
-
-    return render(request, 'bookstore/book_detail.html', content)
+class BookDetail(generic.DetailView):
+    model = Book
+    context_object_name = 'book'
+    template_name = 'bookstore/book_detail.html'
 
 
-def book_create(request):
-    if request.method == 'POST':
-        form = BookCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('bookstore:book_list')
-
-    else:
-        form = BookCreationForm()
-
-    context = {'form': form}
-    return render(request, 'bookstore/book_create.html', context)
+class BookCreate(generic.CreateView):
+    form_class = BookCreationForm
+    context_object_name = 'form'
+    template_name = 'bookstore/book_create.html'
 
 
-def book_update(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    form = BookCreationForm(request.POST or None, instance=book)
-    if form.is_valid():
-        form.save()
-        return redirect(book.get_absolute_url())
-
-    context = {'form': form}
-    return render(request, 'bookstore/book_create.html', context)
+class BookUpdate(generic.UpdateView):
+    model = Book
+    form_class = BookCreationForm
+    context_object_name = 'form'
+    template_name = 'bookstore/book_create.html'
 
 
-def book_delete(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    if request.method == 'POST':
-        book.delete()
-        return redirect('bookstore:book_list')
+class BookDelete(generic.DeleteView):
+    model = Book
+    template_name = 'bookstore/book_delete.html'
+    success_url = reverse_lazy('bookstore:book_list')
 
-    context = {'book': book}
-    return render(request, 'bookstore/book_delete.html', context)
+
+# def book_list(request):
+#     book_lists = Book.objects.filter(active=True).order_by('-datetime_modified')
+#     context = {'book_lists': book_lists}
+#     return render(request, 'bookstore/book_list.html', context)
+
+
+# def book_detail(request, pk):
+#     book = get_object_or_404(Book, pk=pk)
+#
+#     content = {'book': book}
+#
+#     return render(request, 'bookstore/book_detail.html', content)
+
+
+# def book_create(request):
+#     if request.method == 'POST':
+#         form = BookCreationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('bookstore:book_list')
+#
+#     else:
+#         form = BookCreationForm()
+#
+#     context = {'form': form}
+#     return render(request, 'bookstore/book_create.html', context)
+
+
+# def book_update(request, pk):
+#     book = get_object_or_404(Book, pk=pk)
+#     form = BookCreationForm(request.POST or None, instance=book)
+#     if form.is_valid():
+#         form.save()
+#         return redirect(book.get_absolute_url())
+#
+#     context = {'form': form}
+#     return render(request, 'bookstore/book_create.html', context)
+
+
+# def book_delete(request, pk):
+#     book = get_object_or_404(Book, pk=pk)
+#     if request.method == 'POST':
+#         book.delete()
+#         return redirect('bookstore:book_list')
+#
+#     context = {'book': book}
+#     return render(request, 'bookstore/book_delete.html', context)
