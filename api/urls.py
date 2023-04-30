ed60a282fbf95236176ce15e3f0d49bc49639d39
fateme_parand <fateme.parand94@gmail.com>
2023-04-30 20:28:03 +0330

    show book details in REST API

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,5 @@ app_name = 'api'
 
 urlpatterns = [
     path('', views.ApiBookList.as_view(), name='api_book_list'),
+    path('<int:pk>/', views.ApiBookDetail.as_view(), name='api_book_detail'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,12 +1,15 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from bookstore.models import Book
 from .serializers import BookSerializer
 
 
 class ApiBookList(ListAPIView):
-
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+
+class ApiBookDetail(RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
 
